ea77f1cf3359734cd4ec93a07b85a8fafae05256
lilianmueninzioki <liliannzioki@gmail.com>
2024-03-19 23:18:21 +0300

    Function continued JS

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(string = "go to the office"){
+    return `This Monday, I will ${string}.`;
+    
+}
+mondayWork();
+
+function wrapAdjective(lilian = "*") {
+    return function(adjective = "special"){
+        return `You are ${lilian}${adjective}${lilian}!`;
+    }
+
+}
+const encouragingPromptFunction = wrapAdjective("||");
+
+console.log(encouragingPromptFunction("a dedicated programmer"));
+    
+
+    
+
+
